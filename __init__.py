e51f1dc7853f224a5513ef3a697e2968aca76f03
Tomascody-web333 <tomas.afonseca.c@gmail.com>
2026-03-22 12:01:46 +0000

    Update version to 0.2.0 and add __all__ declaration

--- __init__.py
+++ __init__.py
@@ -4,6 +4,6 @@ PyWebSimple - Create websites with only HTML, CSS and Python
 
 from .core import WebApp
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Tomascody-web333"
 __all__ = ["WebApp"]
